adb0ce9d2e180ce553f24434da19c5db7d7e169e
Eslam <113968253+Eslamzaid@users.noreply.github.com>
2023-07-27 16:33:40 +0300

    Update Signup.jsx

--- src/components/notLoged/Signup.jsx
+++ src/components/notLoged/Signup.jsx
@@ -1,20 +1,14 @@
 import { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
-import lessThan from "../../assets/lessThan.png";
+import thmUps from "../../assets/thmUps.png";
 import { useNavigate } from "react-router-dom";
 import { checkIsAuth } from "../../utls/func";
 
-function Signup() {
-  const navigate = useNavigate("");
-
-  const [name, setName] = useState("");
-  const [username, setUserName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [bio, setBio] = useState("");
+function LoginA() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [state, setState] = useState(false);
-  const [mes, setMes] = useState("");
+  const [mes, setMessage] = useState("");
+  const navigate = useNavigate("");
 
   useEffect(() => {
     const checking = async () => {
@@ -26,145 +20,67 @@ function Signup() {
 
     checking();
   });
-  const checkTrue = () => {
-    const fields = [
-      { name: "Name", value: name, minLength: 2, maxLength: 20 },
-      { name: "Username", value: username, minLength: 4, maxLength: 20 },
-      { name: "Phone number", value: phone, minLength: 7 },
-      { name: "Bio", value: bio, minLength: 15, maxLength: 200 },
-      { name: "Email", value: email, minLength: 5 },
-      { name: "Password", value: password, minLength: 6, maxLength: 10 },
-    ];
-    if (!/^\d+$/.test(phone)) {
-      setMes("Phone number should be a NUMBER");
-      return;
-    }
-    if (phone.length > 10) {
-      setMes("Phone number should not be more than 10 numbers");
-      return;
-    }
-
-    for (const field of fields) {
-      if (field.value.length === 0) {
-        setMes(`${field.name} is missing`);
-        return;
-      }
-      if (field.value.length < field.minLength) {
-        setMes(
-          `${field.name} is too short (it should be at least ${field.minLength} characters)`
-        );
-        return;
-      }
-      if (field.maxLength && field.value.length > field.maxLength) {
-        setMes(
-          `${field.name} is too long, it should not be more than ${field.maxLength} characters`
-        );
-        return;
-      }
-    }
-
-    if (!email.includes("@")) {
-      setMes("Invalid email address");
-      return;
-    }
-
-    setMes("");
-    setState(true);
-  };
-
-  const submitForm = async (e) => {
+  const signIn = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:4000/api/signUp", {
+    const response = await fetch("http://localhost:4000/api/login", {
       method: "POST",
       credentials: "include",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email, password, name, username, phone, bio }),
+      body: JSON.stringify({ email, password }),
     });
-    const data = await response.json();
-    if (data.success === false) {
-      setMes(data.message);
-    } else if (data.success === true) {
-      setMes(data.message);
+    const result = await response.json();
+    if ((await result.success) == true) {
       navigate("/home");
+    } else {
+      setEmail("");
+      setPassword("");
+      setMessage(await result.message);
     }
   };
 
+  let dis = false;
+  if (
+    email.length !== 0 &&
+    password.length > 5 &&
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+  ) {
+    dis = true;
+  } else {
+    dis = false;
+  }
+
   return (
-    <section className="m-8 font-snsn">
-      <nav>
-        <img src={logo} alt="Our logo" />
-      </nav>
-      <section className="flex flex-col items-center justify-center mt-10">
-        <div className="w-7/12">
-          {" "}
-          <a href="/login" className=" flex items-center">
-            <span className="w-5">
-              <img src={lessThan} alt="go to login in" />
-            </span>
-            Back to login
+    <div className="grid grid-cols-3 overflow-y-hidden font-snsn bg-[#F0F2F5] h-screen">
+      <div className="col-span-2 m-9 ">
+        <nav className="flex justify-between font-medium">
+          <img src={logo} alt="chory logo " />
+          <a href="/signup" className="hover:underline decoration-stone-700">
+            Don&apos;t have an account?{" "}
+            <span className="text-[#20DC49] font-semibold">Sign up!</span>
           </a>
-        </div>
-        <div className="w-full text-center">
-          <h1 className="text-4xl font-semibold mt-10">Sign up!</h1>
-          <h3 className="text-center text-xl my-6">Create your free account</h3>
-        </div>
-        <form onSubmit={submitForm}>
-          <div className="grid grid-cols-2 gap-x-10">
+        </nav>
+        <article>
+          <section className=" flex justify-center mt-24">
             <div>
-              <div className="mt-2 relative flex justify-center items-center">
-                <input
-                  type="text"
-                  minLength="2"
-                  id="name"
-                  name="name"
-                  autoComplete="name"
-                  placeholder="Name"
-                  className="peer shadow-md sm:w-[21.3rem] h-12 focus:outline-2 focus:outline-stone-500 focus:outline-offset-4 
-                            focus:ring-none placeholder-transparent  focus:ring-0  mb-5 w-8/12 rounded-lg px-2"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-                <label
-                  htmlFor="name"
-                  className="transition-all absolute left-[3%] eexx:left-[4.75rem] -top-7 text-sm w-fit py-2
-                             peer-placeholder-shown:text-base peer-placeholder-shown:top-1 peer-placeholder-shown:text-[#6B7280] peer-focus:-top-7
-                             peer-focus:text-sm peer-focus:text-black"
-                >
-                  Name
-                </label>
-                <br />
-              </div>
-              <div className="mt-2 relative flex justify-center items-center">
-                <input
-                  type="tel"
-                  minLength="7"
-                  id="phone"
-                  name="phone"
-                  autoComplete="phone"
-                  placeholder="Phone"
-                  className="peer shadow-md sm:w-[21.3rem] h-12 focus:outline-2 focus:outline-stone-500 focus:outline-offset-4 
-                            focus:ring-none placeholder-transparent  focus:ring-0  mb-5 w-8/12 rounded-lg px-2"
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                />
-                <label
-                  htmlFor="phone"
-                  className="transition-all absolute left-[3%] eexx:left-[4.75rem] -top-7 text-sm w-fit py-2
-                             peer-placeholder-shown:text-base peer-placeholder-shown:top-1 peer-placeholder-shown:text-[#6B7280] peer-focus:-top-7
-                             peer-focus:text-sm peer-focus:text-black"
-                >
-                  Phone
-                </label>
-                <br />
-              </div>
+              <h2 className=" text-5xl font-semibold">Welcome Back</h2>
+              <h4 className="mb-8 text-center my-5 text-2xl">
+                Login into your account
+              </h4>
+            </div>
+          </section>
+          <section className=" flex justify-center">
+            <form
+              onSubmit={signIn}
+              className=" w-fit  mt-10 flex gap-y-4 flex-col items-center text-left justify-center"
+            >
               <div className="mt-2 relative flex justify-center items-center">
                 <input
                   type="email"
                   id="email"
                   name="email"
-                  autoComplete="email"
+                  autoComplete="username"
                   placeholder="Email"
                   className="peer shadow-md sm:w-[21.3rem] h-12 focus:outline-2 focus:outline-stone-500 focus:outline-offset-4 
                             focus:ring-none placeholder-transparent  focus:ring-0  mb-5 w-8/12 rounded-lg px-2"
@@ -181,63 +97,15 @@ function Signup() {
                 </label>
                 <br />
               </div>
-            </div>
-            <div>
-              <div className="mt-2 relative flex justify-center items-center">
-                <input
-                  type="text"
-                  id="username"
-                  minLength="4"
-                  name="username"
-                  autoComplete="username"
-                  placeholder="username"
-                  className="peer shadow-md sm:w-[21.3rem] h-12 focus:outline-2 focus:outline-stone-500 focus:outline-offset-4 
-                            focus:ring-none placeholder-transparent  focus:ring-0  mb-5 w-8/12 rounded-lg px-2"
-                  value={username}
-                  onChange={(e) => setUserName(e.target.value)}
-                />
-                <label
-                  htmlFor="username"
-                  className="transition-all absolute left-[3%] eexx:left-[4.75rem] -top-7 text-sm w-fit py-2
-                             peer-placeholder-shown:text-base peer-placeholder-shown:top-1 peer-placeholder-shown:text-[#6B7280] peer-focus:-top-7
-                             peer-focus:text-sm peer-focus:text-black"
-                >
-                  username
-                </label>
-                <br />
-              </div>
-              <div className="mt-2 relative flex justify-center items-center">
-                <input
-                  type="text"
-                  id="bio"
-                  minLength="15"
-                  name="bio"
-                  autoComplete="Bio"
-                  placeholder="Bio"
-                  className="peer shadow-md sm:w-[21.3rem] h-12 focus:outline-2 focus:outline-stone-500 focus:outline-offset-4 
-                            focus:ring-none placeholder-transparent  focus:ring-0  mb-5 w-8/12 rounded-lg px-2"
-                  value={bio}
-                  onChange={(e) => setBio(e.target.value)}
-                />
-                <label
-                  htmlFor="bio"
-                  className="transition-all absolute left-[3%] eexx:left-[4.75rem] -top-7 text-sm w-fit py-2
-                             peer-placeholder-shown:text-base peer-placeholder-shown:top-1 peer-placeholder-shown:text-[#6B7280] peer-focus:-top-7
-                             peer-focus:text-sm peer-focus:text-black"
-                >
-                  Bio
-                </label>
-                <br />
-              </div>
-              <div className="mt-2 relative flex justify-center items-center">
+              <div className=" mt-2 relative flex justify-center">
                 <input
                   type="password"
                   id="password"
                   name="password"
-                  autoComplete="Password"
+                  autoComplete="current-password"
                   placeholder="Password"
                   className="peer shadow-md sm:w-[21.3rem] h-12 focus:outline-2 focus:outline-stone-500 focus:outline-offset-4 
-                            focus:ring-none placeholder-transparent  focus:ring-0  mb-5 w-8/12 rounded-lg px-2"
+                  focus:ring-none placeholder-transparent  focus:ring-0  mb-5 w-8/12 rounded-lg px-2"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
@@ -249,24 +117,50 @@ function Signup() {
                 >
                   Password
                 </label>
-                <br />
               </div>
-            </div>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <button
-              className={`w-10/12 mt-10 rounded-lg border-2 border-stone-600 py-3 hover:bg-[#eeeff0] ${"cursor-pointer"}`}
-              type={state ? "submit" : "button"}
-              onClick={checkTrue}
-            >
-              Sing up
-            </button>
-            <p className="text-red-400">{mes}</p>
-          </div>
-        </form>
-      </section>
-    </section>
+              <button
+                className={`sm:w-[21.3rem] w-8/12 rounded-lg border-2 border-stone-600 py-3 hover:bg-[#eeeff0] ${
+                  !dis ? "opacity-50 cursor-auto" : "cursor-pointer"
+                }`}
+                type="submit"
+                disabled={!dis}
+              >
+                Log in
+              </button>
+              <p className="text-red-600 font-medium text-sm">{mes}</p>
+            </form>
+          </section>
+        </article>
+      </div>
+      <div className="col-span-1 bg-black flex flex-col justify-around align-bottom">
+        <div className="text-white mx-4">
+          <h2 className=" text-7xl font-bold ">
+            Keep in touch with{" "}
+            <span className="text-[#20DC49]">your groups</span>
+          </h2>
+          <p className=" text-sm opacity-80 w-96 mt-10 ml-2">
+            With end-to-end encryption, your personal messages and calls are
+            secured. Only you and the person you&apos;re talking to can read or
+            listen to them, and nobody in between, not even WhatsApp.
+          </p>
+          <p className="underline decoration-[#20DC49] underline-offset-4 ml-2 mt-4 opacity-90">
+            Learn more &gt;
+          </p>
+        </div>
+        <div className="mx-16 relative">
+          <div className="bg-[#20DC49] -bottom-36 -left-44 z-30 blur-3xl opacity-20 rounded-full w-[25rem] h-[25rem] absolute"></div>
+          <p className="bg-[#20DC49] w-fit z-10 flex p-4 rounded-xl font-medium">
+            <img className="mr-4" src={thmUps} alt="A thumbs up" /> Top Notch
+            Stock Resources
+          </p>
+          <p className="text-white z-50 mt-10 text-lg">
+            Today, we create innovative solutions to the challenges that
+            consumers face in both their everyday lives and events.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default Signup;
+export default LoginA;
