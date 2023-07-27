e12ad75b955e3746af90ed0f556c36db8a75297e
Eslam <113968253+Eslamzaid@users.noreply.github.com>
2023-07-27 17:59:51 +0300

    Update LoginA.jsx

--- src/components/notLoged/LoginA.jsx
+++ src/components/notLoged/LoginA.jsx
@@ -23,7 +23,7 @@ function LoginA() {
   });
   const signIn = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:4000/api/login", {
+    const response = await fetch("https://chory-b.onrender.com/api/login", {
       method: "POST",
       credentials: "include",
       headers: {
